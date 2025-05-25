642da69771631866f58799f586862a2680d8fc2b
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-05-26 02:40:28 +0700

    feat(db): create NETwebAPI project and add Postgresql into AppHost

--- Aloha/Aloha.AppHost/Program.cs
+++ Aloha/Aloha.AppHost/Program.cs
@@ -1,3 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder.AddPostgres("postgres")
+    .WithImageTag("latest")
+    .WithVolume("postgres-db", "var/lib/postgresql/data") // Mount Docker volume to keep the data when stop the container
+    .WithLifetime(ContainerLifetime.Persistent) // Keep the container running even if the Aspire stops
+    .WithPgAdmin();
+
+builder.AddProject<Projects.Aloha_API>("aloha-api");
+
 builder.Build().Run();
