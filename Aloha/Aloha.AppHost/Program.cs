f23def394909a60f1e9044505eac0898529a3079
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-05-26 03:03:39 +0700

    build(aspire): update Aspire from 9.0.0 to 9.3.0

--- Aloha/Aloha.AppHost/Program.cs
+++ Aloha/Aloha.AppHost/Program.cs
@@ -4,7 +4,11 @@ var postgres = builder.AddPostgres("postgres")
     .WithImageTag("latest")
     .WithVolume("postgres-db", "var/lib/postgresql/data") // Mount Docker volume to keep the data when stop the container
     .WithLifetime(ContainerLifetime.Persistent) // Keep the container running even if the Aspire stops
-    .WithPgAdmin();
+    .WithPgAdmin(pgBuilder => {
+        pgBuilder.WithImageTag("latest");
+    });
+
+var AlohaDb = postgres.AddDatabase("aloha-db","aloha");
 
 builder.AddProject<Projects.Aloha_API>("aloha-api");
 
