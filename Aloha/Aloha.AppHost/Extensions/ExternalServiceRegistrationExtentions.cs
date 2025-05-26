3cd397182f0ad76425355d15ebc7bbb699e00846
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-05-27 00:23:19 +0700

    refactor(aspire-app-host): centralize the Aspire Resource Register
    Updated `Aloha.AppHost.csproj` to include references to `Aloha.ApiGateway.csproj` and
    `Aloha.API.csproj`
    Introduced `ExternalServiceRegistrationExtentions` class with
    `AddApplicationServices` method for PostgreSQL service configuration
    Refactored `Program.cs` to
    simplify service registration by calling the new method, enhancing code organization and readability
    
    BREAKING CHANGE: Resource Registing will be in the ExternalServiceRegistrationExtentions

---
+++ Aloha/Aloha.AppHost/Extensions/ExternalServiceRegistrationExtentions.cs
@@ -0,0 +1,32 @@
+﻿using Aspire.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Json;
+
+
+namespace Aloha.AppHost.Extensions
+{
+    public static class ExternalServiceRegistrationExtentions
+    {
+        public static IDistributedApplicationBuilder AddApplicationServices(this IDistributedApplicationBuilder builder)
+        {
+            //var cache = builder.AddRedis("redis");
+            //var rabbitMq = builder.AddRabbitMq("rabbitmq");
+
+            var postgres = builder.AddPostgres("postgres")
+                                  .WithImageTag("latest")
+                                  .WithVolume("postgres-db", "var/lib/postgresql/data") // Mount Docker volume to keep the data when stop the container
+                                  .WithLifetime(ContainerLifetime.Persistent) // Keep the container running even if the Aspire stops
+                                  .WithPgAdmin(pgBuilder =>{
+                                      pgBuilder.WithImageTag("latest");
+                                  });
+
+            var AlohaDb = postgres.AddDatabase("aloha-db", "aloha");
+
+            builder.AddProjectWithPostfix<Projects.Aloha_API>();
+
+            builder.AddProjectWithPostfix<Projects.Aloha_ApiGateway>();
+
+            return builder;
+        }
+    }
+}
--- Aloha/Aloha.AppHost/Program.cs
+++ Aloha/Aloha.AppHost/Program.cs
@@ -1,15 +1,7 @@
+using Aloha.AppHost.Extensions;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres")
-    .WithImageTag("latest")
-    .WithVolume("postgres-db", "var/lib/postgresql/data") // Mount Docker volume to keep the data when stop the container
-    .WithLifetime(ContainerLifetime.Persistent) // Keep the container running even if the Aspire stops
-    .WithPgAdmin(pgBuilder => {
-        pgBuilder.WithImageTag("latest");
-    });
-
-var AlohaDb = postgres.AddDatabase("aloha-db","aloha");
-
-builder.AddProject<Projects.Aloha_API>("aloha-api");
+builder.AddApplicationServices();
 
 builder.Build().Run();
