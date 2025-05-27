27e76bde21b432a76597bec6c226216fd1c7b76c
DuongNManh <manhduonglhp4@gmail.com>
2025-05-27 22:50:03 +0700

    feat: add location service

--- Aloha/Aloha.AppHost/Program.cs
+++ Aloha/Aloha.AppHost/Program.cs
@@ -4,4 +4,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddApplicationServices();
 
+builder.AddProject<Projects.Aloha_LocationService>("aloha-locationservice");
+
 builder.Build().Run();
