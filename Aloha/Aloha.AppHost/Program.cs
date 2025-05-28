e9841fbdd789f27d52b093f32156cb2123e7203b
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-05-28 07:18:31 +0700

    refactor(aspire_resource): move AspireResource register into ServiceRegistrationExtentions
    Updated `ApplicationServiceExtensions` to register new projects: `Aloha_API`, `Aloha_EventBus`, and
    `Aloha_LocationService`. Removed the previous default database registration. Adjusted `Program.cs`
    to utilize the new helper methods for adding application services, removing direct project additions
    for `Aloha_LocationService` and `Aloha_EventBus`.

--- Aloha/Aloha.AppHost/Program.cs
+++ Aloha/Aloha.AppHost/Program.cs
@@ -4,8 +4,4 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddApplicationServices();
 
-builder.AddProject<Projects.Aloha_LocationService>("aloha-locationservice");
-
-builder.AddProject<Projects.Aloha_EventBus>("aloha-eventbus");
-
 builder.Build().Run();
