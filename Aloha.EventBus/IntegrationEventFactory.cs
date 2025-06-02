950c86d754e5f92ba9df3873720965ceef5ff279
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-06-02 15:32:53 +0700

    feat(eventbus): add generic IntegrationEventFactory<TEvent> for assembly-scoped type resolution

--- Aloha.EventBus/IntegrationEventFactory.cs
+++ Aloha.EventBus/IntegrationEventFactory.cs
@@ -4,8 +4,9 @@ using System.Reflection;
 using System.Text.Json;
 
 namespace Aloha.EventBus;
+
 public class IntegrationEventFactory : IIntegrationEventFactory
-{   
+{
     public IntegrationEvent? CreateEvent(string typeName, string value)
     {
         var t = GetEventType(typeName) ?? throw new ArgumentException($"Type {typeName} not found");
@@ -21,4 +22,29 @@ public class IntegrationEventFactory : IIntegrationEventFactory
     }
 
     public static readonly IntegrationEventFactory Instance = new();
+}
+
+
+public class IntegrationEventFactory<TEvent> : IIntegrationEventFactory
+{
+    private static readonly Assembly integrationEventAssembly = typeof(TEvent).Assembly;
+
+    public IntegrationEvent? CreateEvent(string typeName, string value)
+    {
+        var t = GetEventType(typeName) ?? throw new ArgumentException($"Type {typeName} not found");
+
+        return JsonSerializer.Deserialize(value, t) as IntegrationEvent;
+    }
+
+    private static Type? GetEventType(string type)
+    {
+        // most of the time the type will be in CQRS.Library.IntegrationEvents assembly
+        var t = integrationEventAssembly.GetType(type) ?? Type.GetType(type);
+
+        return t ?? AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(a => a.GetTypes())
+            .FirstOrDefault(t => t.FullName == type);
+    }
+
+    public static readonly IntegrationEventFactory<TEvent> Instance = new();
 }
