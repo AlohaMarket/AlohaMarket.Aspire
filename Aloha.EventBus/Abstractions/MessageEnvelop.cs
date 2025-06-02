dd171842308d44b7727da1411c03117c5dffe917
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-06-02 11:15:44 +0700

    feat(evenbus): add constructors to MessageEnvelop for improved initialization

--- Aloha.EventBus/Abstractions/MessageEnvelop.cs
+++ Aloha.EventBus/Abstractions/MessageEnvelop.cs
@@ -2,5 +2,22 @@
 {
     public class MessageEnvelop
     {
+        public MessageEnvelop()
+        {
+        }
+
+        public MessageEnvelop(Type type, string message) : this(type.FullName!, message)
+        {
+        }
+
+        public MessageEnvelop(string messageTypeName, string message)
+        {
+            MessageTypeName = messageTypeName ?? throw new ArgumentNullException(nameof(messageTypeName));
+            Message = message ?? throw new ArgumentNullException(nameof(message));
+        }
+
+        public string MessageTypeName { get; set; } = default!;
+
+        public string Message { get; set; } = default!;
     }
 }
