02f56234facc1d5fe007c7b087202d8b8eb732a7
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-06-03 18:15:11 +0700

    feat(kafka): add Kafka support into AspireAppHost
    Updated `Aloha.AppHost.csproj` to include package references for `Aspire.Hosting.Kafka`, along with
    a project reference for `Aloha.EventBus.Kafka`.
    Modified `AddApplicationServices` in
    `ExternalServiceRegistrationExtentions.cs` to integrate Kafka, enhancing service dependencies by
    including `locationService` and `categoryService`.
    Removed the direct project addition for the
    Kafka event bus in `Program.cs`, streamlining service registration.

--- Aloha/Aloha.AppHost/Program.cs
+++ Aloha/Aloha.AppHost/Program.cs
@@ -4,6 +4,4 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddApplicationServices();
 
-builder.AddProject<Projects.Aloha_EventBus_Kafka>("aloha-eventbus-kafka");
-
 builder.Build().Run();
