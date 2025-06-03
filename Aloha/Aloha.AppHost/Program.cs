d3e7851c555ed925d24ea11135ecdfd20477c1aa
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-06-03 17:31:01 +0700

    feat(kafka): add Aloha.EventBus.Kafka project and related updates
    - Updated solution file to include new project `Aloha.EventBus.Kafka`.
    - Added build configurations
    for the new project in Debug and Release modes.
    - Created `Aloha.EventBus.Kafka.csproj` targeting
    .NET 9.0 with necessary properties and project reference.
    - Modified `Program.cs` to initialize web
    app with service defaults and updated root endpoint response to "Hello Kafka!".
    - Added `launchSet

--- Aloha/Aloha.AppHost/Program.cs
+++ Aloha/Aloha.AppHost/Program.cs
@@ -4,4 +4,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddApplicationServices();
 
+builder.AddProject<Projects.Aloha_EventBus_Kafka>("aloha-eventbus-kafka");
+
 builder.Build().Run();
