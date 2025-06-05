8ac541d1d3e926221927f4a3350a0487bb19d4c3
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-06-05 13:35:05 +0700

    feat: Add Chat, Conversation

---
+++ Aloha.NotificationService/Models/DTOs/UserDto.cs
@@ -0,0 +1,12 @@
+namespace Aloha.NotificationService.Models.DTOs
+{
+    public class UserDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Avatar { get; set; } = string.Empty;
+        public bool IsOnline { get; set; }
+        public DateTime LastSeenAt { get; set; }
+    }
+} 
