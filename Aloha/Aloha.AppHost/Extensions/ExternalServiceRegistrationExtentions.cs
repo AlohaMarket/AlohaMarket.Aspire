60ed2218ca71db86dc4dcbd66f899f4619c4a638
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-06-08 01:37:51 +0700

    refactor(AppHost): Remove EventBus references, Postgres Container from AppHost configuration

--- Aloha/Aloha.AppHost/Extensions/ExternalServiceRegistrationExtentions.cs
+++ Aloha/Aloha.AppHost/Extensions/ExternalServiceRegistrationExtentions.cs
@@ -6,42 +6,20 @@ public static class ApplicationServiceExtensions
 {
     public static IDistributedApplicationBuilder AddApplicationServices(this IDistributedApplicationBuilder builder)
     {
-        var postgres = builder.AddPostgres("postgres");
         var kafka = builder.AddKafka("kafka");
 
         if (!builder.Configuration.GetValue("IsTest", false))
         {
-            postgres = postgres.WithLifetime(ContainerLifetime.Persistent)
-                .WithVolume("postgres-db", "var/lib/postgresql/data")
-                .WithPgAdmin(pgBuilder =>
-                {
-                    pgBuilder.WithImageTag("latest");
-                });
-
             kafka = kafka.WithLifetime(ContainerLifetime.Persistent).WithDataVolume().WithKafkaUI();
         }
-
-        // Add application services using helper methods
-        var alohaDb = postgres.AddDefaultDatabase<Projects.Aloha_API>();
-
-        var apiService = builder.AddProjectWithPostfix<Projects.Aloha_API>()
-            .WithReference(postgres)
-            .WithReference(alohaDb, "DefaultConnection");
-
-
-        var eventBus = builder.AddProjectWithPostfix<Projects.Aloha_EventBus>();
-
-        //var ;
 
         var locationService = builder.AddProjectWithPostfix<Projects.Aloha_LocationService>();
 
         var categoryService = builder.AddProjectWithPostfix<Projects.Aloha_CategoryService>();
 
         var gatewayService = builder.AddProjectWithPostfix<Projects.Aloha_ApiGateway>()
-            .WithReference(apiService)
             .WithReference(locationService)
-            .WithReference(categoryService)
-            ;
+            .WithReference(categoryService)            ;
 
         return builder;
     }
