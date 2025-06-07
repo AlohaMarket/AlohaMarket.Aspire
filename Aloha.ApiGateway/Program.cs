2f6a6b205103a63a9b5c0a6fd8a9a5bd55063b69
DuongNManh <manhduonglhp4@gmail.com>
2025-06-07 19:48:25 +0700

    feat(userservice): userService Implement
    BREAKING CHANGE: implement user service

--- Aloha.ApiGateway/Program.cs
+++ Aloha.ApiGateway/Program.cs
@@ -1,4 +1,3 @@
-using Aloha.ApiGateway.Services;
 using Aloha.ServiceDefaults.DependencyInjection;
 using Aloha.ServiceDefaults.Hosting;
 
@@ -18,14 +17,13 @@ builder.Services.AddCors(options =>
 
 // Core services
 builder.Services.AddControllers();
-builder.Services.AddSingleton<SwaggerMergeService>();
 builder.Services.AddHttpClient();
 builder.Services.AddSwaggerGen();
 
 // YARP Reverse Proxy
 AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 builder.Services.AddReverseProxy()
-       .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 // Auth (Keycloak)
 builder.Services.AddJwtAuthentication(builder.Configuration);
@@ -44,22 +42,6 @@ app.UseAuthorization();
 app.MapControllers();
 app.MapReverseProxy();
 app.UseSwagger();
-
-// Serve merged Swagger JSON
-app.MapGet("/swagger/v1/merged.json", async (SwaggerMergeService merger) =>
-{
-    var doc = await merger.GetMergedSwaggerAsync();
-
-    var stream = new MemoryStream();
-    var writer = new StreamWriter(stream);
-    var openApiWriter = new Microsoft.OpenApi.Writers.OpenApiJsonWriter(writer);
-
-    doc.SerializeAsV3(openApiWriter);
-    await writer.FlushAsync();
-    stream.Position = 0;
-
-    return Results.Stream(stream, "application/json");
-});
 
 // Swagger UI using merged doc only
 app.UseSwaggerUI(c =>
